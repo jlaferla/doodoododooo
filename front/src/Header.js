ac6049846f6d0ab5bc7e4c7a172c515e70d694de
jlaferla <jehlaferla@gmail.com>
2025-04-18 14:54:45 +0400

    Latest version

--- front/src/Header.js
+++ front/src/Header.js
@@ -1,19 +1,22 @@
 // Header.js
 import React from 'react';
-import logo from './logo.png';  // Make sure logo.png is in the same folder as Header.js (or adjust the path)
+import logo from './logo.png';
 import { Link } from 'react-router-dom';
-import './Header.css'; // (or App.css if you prefer)
+import './Header.css';
 
 export default function Header() {
   return (
     <header className="header-container">
       <div className="branding">
-      <img src={logo} alt="FX Ping Logo" className="site-logo" />
+        <img src={logo} alt="FX Ping Logo" className="site-logo" />
         <span className="site-tagline">Simple Currency Converter</span>
       </div>
+
+      {/* now its own element */}
       <nav className="nav-links">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>      </nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
     </header>
   );
 }
